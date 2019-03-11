07338287fc88f220f6e1bfbe72cb28ee4f57acee
fernando.leon <fEr@fErMBP.local>
2019-03-11 19:37:28 +0100

    Aportación de fleong, Fernando León.

--- ml-data.py
+++ ml-data.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from ninstances_py.ninstances import ninstances
+from mld_statistics_py.mld_statistics import MdlSta
 
 def main():
     loop_condition=True
@@ -9,6 +10,7 @@ def main():
         print("\nMenu:\n")
         print("0.- EXIT")
         print("1.- Number of instances, features and labels (Juan A. Romero)")
+        print("2.- Multi-label Datasets and Statistics (Fernando León")
 
         main_input = int(input())
 
@@ -19,7 +21,9 @@ def main():
             datafile=input("Introduce nombre de datafile: ")
             ninstances(datafile)
         elif main_input == 2:
-            print("add yours here")
+            datafile = input("Introduce nombre de datafile: ")
+            MdlSta(datafile)
+
 
 
 if __name__ == "__main__":
