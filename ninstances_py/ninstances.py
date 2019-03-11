506dc32361b41b758e68f1ab2c6035bc79b9a715
F. Javier Peñas Pastilla <javielpasti96@gmail.es>
2019-03-11 22:43:32 +0100

    Introduccion de conteo de ocurrencias

--- ninstances_py/ninstances.py
+++ ninstances_py/ninstances.py
@@ -11,3 +11,22 @@ def ninstances(datafilename):
     print("datafile: ", datafilename, "instances: ", instances, "features: ",
           features, "labels: ", labels, "\n")
     datafile.close()
+
+def occLabels(datafilename):
+    datafile=open("data/"+datafilename)
+
+    instances=datafile.readline()
+    datafile.readline()
+    labels=datafile.readline()
+    instances=int(instances.split()[1])
+    labels=int(labels.split()[1])
+	occurrences = [0]*labels
+
+	for counter in range(instances):
+		line = datafile.readline()
+		aux=line.split(" ")
+		for i in range(labels):
+			occurrences[i]= occurrences[i]+int(aux[i+len(aux)-labels])
+
+    datafile.close()
+	return occurrences
